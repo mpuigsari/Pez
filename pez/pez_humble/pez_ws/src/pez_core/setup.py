141db4ceee1ec2929cee640b8872e7635984a060
mpuigsari <al415556@uji.es>
2025-05-15 13:34:38 +0200

    Missed setup.py

---
+++ pez/pez_humble/pez_ws/src/pez_core/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+package_name = 'pez_core'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        # ament index entry
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        # package.xml
+        ('share/' + package_name, ['package.xml']),
+        # config files (YAML and RQt perspective)
+        ('share/' + package_name + '/config', [
+            'config/joystick_params.yaml',
+            'config/axis_params.yaml',
+            'config/pez.perspective',
+        ]),
+        # launch files
+        ('share/' + package_name + '/launch', [
+            'launch/joy_launch.py',
+            'launch/teleop_launch.py',
+            'launch/joy_launch.py',
+        ]),
+    ],
+    install_requires=['setuptools', 'bluerobotics-navigator'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='Pez control nodes ported to ROS 2 Humble',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'fish_teleop = pez_core.fish_teleop:main',
+            'fish_joy        = pez_core.fish_joy:main',
+        ],
+    },
+)
