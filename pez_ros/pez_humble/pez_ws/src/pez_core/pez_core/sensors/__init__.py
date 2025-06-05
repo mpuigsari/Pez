527047dcc0f2d4dfc27de7ca96617ca90a21d099
mpuigsari <al415556@uji.es>
2025-06-05 18:28:44 +0200

    Pez_core: Fix init sensors

--- pez_ros/pez_humble/pez_ws/src/pez_core/pez_core/sensors/__init__.py
+++ pez_ros/pez_humble/pez_ws/src/pez_core/pez_core/sensors/__init__.py
@@ -1,7 +1,14 @@
-from .tsys01   import TSYS01
-from .ms5837   import MS5837, MODEL_30BA
+# pez_core/sensors/__init__.py
+
+# Expose SensorManager from the sensor_manager.py module
+from .sensor_manager import SensorManager
+
+# And if you still need to expose the raw drivers, you can also:
+from .tsys01 import TSYS01
+from .ms5837 import MS5837, MODEL_30BA
 
 __all__ = [
+    'SensorManager',
     'TSYS01',
     'MS5837',
     'MODEL_30BA',
