976a20111ae38f491c651e19311f639867455165
mpuigsari <al415556@uji.es>
2025-06-05 18:21:13 +0200

    Pez_core: Fix init sensors

--- pez_ros/pez_humble/pez_ws/src/pez_core/pez_core/sensors/__init__.py
+++ pez_ros/pez_humble/pez_ws/src/pez_core/pez_core/sensors/__init__.py
@@ -1,11 +1,8 @@
-# pez_core/sensors/__init__.py
-
-from .tsys01_sensor import TSYS01Sensor
-from .ms5837_sensor import MS5837Sensor
-from .sensor_manager import SensorManager
+from .tsys01   import TSYS01
+from .ms5837   import MS5837, MODEL_30BA
 
 __all__ = [
-    'TSYS01Sensor',
-    'MS5837Sensor',
-    'SensorManager',
+    'TSYS01',
+    'MS5837',
+    'MODEL_30BA',
 ]
