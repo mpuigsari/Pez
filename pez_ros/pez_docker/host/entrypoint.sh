c120de07ffdb1db8d64de3a9a52eecc1e7f5b67d
mpuigsari <al415556@uji.es>
2025-06-09 12:57:36 +0200

    Pez_docker: Organize dockers

---
+++ pez_ros/pez_docker/host/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# 1) Make all newly created files world-writable/readable
+umask 000
+
+# 2) Source ROS 2 Humble environment
+source /opt/ros/humble/setup.bash
+
+# 3) Source the workspace (if it's already been built)
+if [ -f /pez_ws/install/setup.bash ]; then
+  source /pez_ws/install/setup.bash
+fi
+
+# 4) Move into your workspace (the bound host pez_ws)
+cd /pez_ws
+
+# 5) Exec the given command (defaults to `bash` in your compose)
+exec "$@"
